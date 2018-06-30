fe62be0b45e25adc58f81030ac5e0a46af0bcdda
Andrew A. Anissi <andrew@wingedearth.com>
2018-06-29 17:17:26 -0700

    Add webpack (#3)
    * add webpack with backend build
    
    * add babelrc and eslintrc changes
    
    * exclude node modules from server bundle
    
    * add_webpack

---
+++ webpack/config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const helpers = require('./helpers');
+const rules = require('./rules');
+
+const config = {
+	mode: helpers.isProd() ? 'production' : 'development',
+	node: {
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty'
+	},
+	output: {
+		publicPath: path.resolve(__dirname, '..', 'build'),
+		filename: '[name].js',
+		path: path.resolve(__dirname, '..', 'build')
+	},
+	resolve: {
+		modules: [path.join(__dirname, '..', 'src'), 'node_modules'],
+		extensions: ['.js', '.es6', '.json', '.css']
+	},
+	module: {
+		rules: rules
+	},
+	watchOptions: {
+		ignored: /node_modules/,
+		poll: true
+	}
+};
+
+module.exports = config;
