a7db30c6aca59a3b3d5c2ad0da7e48ea5d4c910d
Andrew A. Anissi <andrew@wingedearth.com>
2018-07-04 15:11:20 -0700

    Add MongoDB and Spotify API logic for searching, storing, and retrieving (#4)

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import path from 'path';
 import router from './routes/router';
 
 require('babel-polyfill');
+require('./config/database');
 
 // constants
 const DEFAULT_PORT = 3000;
 
 const server = express();
-const port = DEFAULT_PORT;
+const port = process.env.PORT || DEFAULT_PORT;
 
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static(path.resolve(__dirname, '..', '..')));
 server.use(express.static('./build'));
-
 server.use(router);
 
 server.listen(port, () => console.log(`A goblin-mage has conjured a server on port ${port}.`));
